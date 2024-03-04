6d292963b4d025881b36cdc70fdaae858a756342
Anonimo93 <zandytorrea@gmail.com>
2024-03-04 18:40:39 +0000

    change structure

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import fs from "fs/promises";
 import { execSync } from "child_process";
 import moment from "moment";
 
-import { updateStatus } from "./updateStatus.js";
-import { updateConnection } from "./updateConnection.js"
+import { updateStatus } from "./Modules/updateStatus.js";
+import { updateConnection } from "./Modules/updateConnection.js"
 
 async function main(){
     await updateConnection("Anonimo93");
